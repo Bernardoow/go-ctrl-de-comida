32956de98c6834c273a24c0cfb298c8d2028485e
Bernardo Gomes <bgomesdeabreu@gmail.com>
2017-11-26 19:07:33 -0200

    Add env port

--- main.go
+++ main.go
@@ -7,15 +7,28 @@ import (
     _ "github.com/lib/pq"
     _ "app/models"
     "os"
+    "strconv"
+    "fmt"
 )
 
 func init() {
     orm.RegisterDriver("postgres", orm.DRPostgres)
     // orm.RegisterDataBase("default", "postgres", "user=postgres password=postgres host=127.0.0.1 port=5432 dbname=postgres sslmode=disable")
     orm.RegisterDataBase("default", "postgres", os.Getenv("DATABASE_URL"))
+
 }
 
 func main() {
+
+    port, err := strconv.Atoi(os.Getenv("PORT"));
+
+    if err != nil {
+        port = 8000
+    }
+
+    fmt.Println(port)
+
+    beego.BConfig.Listen.HTTPPort = port
     beego.Run()
 }
 
